cb935a472cfd84cda304fb70368f120cc357d036
Lawrence Malloy <lcmalloy@gmail.com>
2022-12-02 17:58:07 -0800

    updated text in SongCard for title and artist

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -14,6 +14,18 @@ const SongCard = ({ song, i }) => {
         </div>
         <img alt="song_img" src={song.images?.coverart} />
       </div>
+      <div className="mt-4 flex flex-col">
+        <p className="font-semibold text-lg text-white truncate">
+          <Link to={`/songs/${song?.key}`}>
+            {song.title}
+          </Link>
+        </p>
+        <p className="text-sm truncate text-gray-300 mt-1">
+          <Link to={song.artists ? `/artists/${song?.artists[0]?.adamid}` : '/top-artists'}>
+            {song.subtitle}
+          </Link>
+        </p>
+      </div>
     </div>
   )
 }
