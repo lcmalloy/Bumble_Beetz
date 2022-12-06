8b8748457d40a0762350721b058d04c52c528d07
Lawrence Malloy <lcmalloy@gmail.com>
2022-12-06 15:08:34 -0800

    added geolocation using Ipify.org Implemented Around You Page

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -13,8 +13,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
     endpoints: (builder) => ({
       getTopCharts: builder.query({ query: () => '/charts/world' }),
       getSongDetails: builder.query({ query: ({songid}) => `/tracks/details?track_id=${songid}`}),
-      getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}`})
+      getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}`}),
+      getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+      getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}`})
     })
   })
 
-  export const { useGetTopChartsQuery, useGetSongDetailsQuery, useGetSongRelatedQuery } = shazamCoreApi;
+  export const {
+    useGetTopChartsQuery,
+    useGetSongDetailsQuery,
+    useGetSongRelatedQuery,
+    useGetArtistDetailsQuery,
+    useGetSongsByCountryQuery,
+  } = shazamCoreApi;
