8e07b4b867a7a199424c26d8580d837e02260f2e
Lawrence Malloy <lcmalloy@gmail.com>
2022-12-06 15:52:00 -0800

    implemented search component added filter functionality for searching genres

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,14 +2,14 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import { Error, Loader, SongCard } from '../components'
 import { genres } from '../assets/constants.js'
-
-import {useGetTopChartsQuery} from '../redux/services/shazamCore.js'
+import { selectGenreListId } from '../redux/features/playerSlice.js'
+import { useGetSongsByGenreQuery } from '../redux/services/shazamCore.js'
 
 const Discover = () => {
   const dispatch = useDispatch()
-  const { activeSong, isPlaying } = useSelector((state) => state.player)
-  const { data, isFetching, error } = useGetTopChartsQuery();
-  const genreTitle = 'Pop';
+  const { activeSong, isPlaying, genreListId } = useSelector((state) => state.player)
+  const { data, isFetching, error } = useGetSongsByGenreQuery(genreListId || 'POP');
+  const genreTitle = genres.find(({ value }) => value === genreListId)?.title;
 
   if (isFetching) return <Loader title="Loading songs..." />;
 
@@ -20,8 +20,8 @@ return (
     <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
       <h2 className="font-bold text-3xl text-white text-left">Discover {genreTitle} </h2>
       <select
-        onChange={() => {}}
-        value=""
+        onChange={(e) => dispatch(selectGenreListId(e.target.value))}
+        value={genreListId || 'pop'}
         className="bg-black text-gray-300 p-3 text-sim rounded-lg outline-none sm:mt-0 mt-5"
       >
         {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
